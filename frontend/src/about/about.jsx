306ca10e6b4976a569150e17a83ccd5970b5b9fa
paulo.henrique <paulo.henrique@softplan.com.br>
2019-02-10 00:17:34 -0200

    Aplicação todo react

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós' />
+
+    <h2>Nossa Historia</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nihil repellat quis voluptate neque illo dolores quia maxime. Hic necessitatibus perspiciatis, maiores eius repellat numquam in ex! Iusto, non ullam!</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, facere, voluptate mollitia minus deleniti magni eaque architecto vero veritatis reiciendis perferendis, fugit laboriosam odit eveniet dolore corrupti amet obcaecati quod!</p>
+    <h2>Impressa</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tenetur dolor deserunt reiciendis architecto dicta harum pariatur nisi, totam omnis cumque, doloribus ea cupiditate natus a nam debitis neque minima!</p>
+  </div>
+)
